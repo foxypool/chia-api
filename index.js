e5772496e7e45ba036b6cbb83152f9c86b2d1bf6
Felix Brucker <felix@felixbrucker.com>
2021-04-18 08:26:57 +0200

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+const Connection = require('./lib/connection');
+const Api = require('./lib/api');
+const constants = require('./lib/constants');
+
+module.exports = {
+  Connection,
+  Api,
+  constants,
+};
